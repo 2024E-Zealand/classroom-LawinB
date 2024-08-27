477a2c5d8d09b1c18607560c621d42dd77fbf897
Lawin Blend Bamarne <lab002@edu.zealand.dk>
2024-08-27 13:48:03 +0200

    Added classroom class

---
+++ ClassRoomNet60/Classroom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class Classroom
+    {
+        public string Classname { get; private set; }
+        public List <student> studentlist { get; private set; }
+        public DateTime semesterstart { get; private set; }
+
+        public Classroom(string classname, List<student> studentlist, DateTime semesterstart)
+        {
+            Classname = classname;
+            this.studentlist = studentlist;
+            this.semesterstart = semesterstart;
+        }
+    }
+}
