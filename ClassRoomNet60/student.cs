32da3f0717eec47d764738af1768462f688a1b35
Lawin Blend Bamarne <lab002@edu.zealand.dk>
2024-08-27 13:38:33 +0200

    Added student class

---
+++ ClassRoomNet60/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class student
+    {
+        public string name {  get; private set; }
+        public int birthmonth { get; private set; }
+        public int birthday { get; private set; }
+    }
+}
